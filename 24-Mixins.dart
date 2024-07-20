c8d07e1d6a55db36d3b1a5af1b4b9914ba7a88f4
Rajiv Iyer <rajivpiyer@gmail.com>
2024-07-20 22:03:06 +0530

    commit

--- 24-Mixins.dart
+++ 24-Mixins.dart
@@ -1,1 +1,39 @@
-// Mixins
+// class Animal with Walk, Talk, Reproduce {
+//   int legs = 0;
+
+// }
+
+mixin Walk {
+  bool canWalk = false;
+  void walk() {
+    if (canWalk) {
+      print("Can Walk");
+    }
+  }
+}
+
+mixin Talk {
+  bool canTalk = false;
+  void talk() {
+    if (canTalk) {
+      print("Can Talk");
+    }
+  }
+}
+
+mixin Reproduce {
+  bool canReproduce = false;
+  void reproduce() {
+    if (canReproduce) {
+      print("Can Reproduce");
+    }
+  }
+}
+
+class Human with Walk, Talk, Reproduce {
+  canWalk = true;
+}
+void main() {
+  Human h = Human();
+  
+}
